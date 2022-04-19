ed4191529eca75c46db38b8f1275429d568aa784
Sabrina Topel <sabrina.f.topel@gmail.com>
2022-04-19 11:29:16 -0300

    first commit

--- rh/src/app/app.module.ts
+++ rh/src/app/app.module.ts
@@ -1,45 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {
-  NgxAwesomePopupModule,
-  DialogConfigModule,
-  ConfirmBoxConfigModule,
-  ToastNotificationConfigModule
-} from '@costlydeveloper/ngx-awesome-popup';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FichaComponent } from './components/cadastro/ficha/ficha.component';
-import {MatTableModule} from '@angular/material/table';
-import {MatIconModule} from '@angular/material/icon';
-import { NgxMaskModule } from 'ngx-mask';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSortModule } from '@angular/material/sort';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FichaUpdateComponent } from './components/update/ficha-update/ficha-update.component';
 import { TextMaskModule } from 'angular2-text-mask';
 import { TabelaColaboradoresComponent } from './components/home/tabela-colaboradores/tabela-colaboradores.component';
+
+import {MatDialogModule} from '@angular/material/dialog';
 @NgModule({
   declarations: [
     AppComponent,
     FichaComponent,
     FichaUpdateComponent,
     TabelaColaboradoresComponent,
-
-
   ],
   imports: [
+    MatDialogModule,
+    MatSortModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
@@ -61,16 +55,9 @@ import { TabelaColaboradoresComponent } from './components/home/tabela-colaborad
     FormsModule,
     MatIconModule,
     TextMaskModule,
-    NgxMaskModule.forRoot(),
-    NgxAwesomePopupModule.forRoot(), // Essential, mandatory main module.
-    DialogConfigModule.forRoot(), // Needed for instantiating dynamic components.
-    ConfirmBoxConfigModule.forRoot(), // Needed for instantiating confirm boxes.
-    ToastNotificationConfigModule.forRoot(), // Needed for instantiating toast notifications.
-
-  
     MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- rh/src/app/components/home/tabela-colaboradores/tabela-colaboradores.component.ts
+++ rh/src/app/components/home/tabela-colaboradores/tabela-colaboradores.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Sort } from '@angular/material/sort';
 import { DadosPessoais, DadosProfissionais } from 'src/app/siscrh';
 import { SiscrhService } from 'src/app/siscrh.service';
 
@@ -8,23 +10,53 @@ import { SiscrhService } from 'src/app/siscrh.service';
   styleUrls: ['./tabela-colaboradores.component.css'],
 })
 export class TabelaColaboradoresComponent implements OnInit {
-  constructor(private siscrhService: SiscrhService) {}
-  DadosPessoais: DadosPessoais[];
-  DadosProfissionais: DadosProfissionais[];
+  constructor(private siscrhService: SiscrhService) {
+  }
+  filtro = new FormControl();
+
 
   array: any =[];
+  teste:any
 
   ngOnInit(): void {
     //  JUNTAR OS DOIS DADOS EM UM ARRAY
 
     this.siscrhService.getDadosProfissionaisList().subscribe((data: any) => {
-      this.DadosProfissionais = data;
-      /* console.log(data) */
-
       for (let i in data) {
         this.array.push({id:data[i].id, nome:[data[i].dadosPessoais], setor:[data[i].setores], situacao:[data[i].situacaoColaborador]})
       }
       console.log(this.array);
     });
   }
+  sortData(sort: Sort) {
+    const data = this.array.slice();
+    if (!sort.active || sort.direction === '') {
+      this.array = data;
+      return;
+    }
+
+    this.array = data.sort((a:any, b:any) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'nome':
+          return compare(a.nome, b.nome, isAsc);
+        case 'setor':
+          return compare(a.setor, b.setor, isAsc);
+        case 'situacao':
+          return compare(a.situacao, b.situacao, isAsc);
+        case 'acessoRede':
+          return compare(a.acessoRede, b.acessoRede, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+  editContact(id:number) {
+   console.log(id)
+
+  }
+ 
+}
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
